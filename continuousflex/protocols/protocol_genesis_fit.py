916062ffcdfcfcdd59b88ad68f184c76c7ba8433
ubuntu-laptop <rvuillemot@gmail.com>
2021-07-09 13:37:22 +0200

    test

--- continuousflex/protocols/protocol_genesis_fit.py
+++ continuousflex/protocols/protocol_genesis_fit.py
@@ -294,6 +294,7 @@ class FlexProtGenesisFit(ProtAnalysis3D):
         os.system("cp %s %s" %(self.inputGenesisMin.get()._getExtraPath("min.rst"), self._getExtraPath("")))
         os.system("cp %s %s" %(self.inputGenesisMin.get().inputPSF.get().getFileName(), self._getExtraPath("")))
         os.system("cp %s %s" %(self.inputGenesisMin.get().inputPDB.get().getFileName(), self._getExtraPath("")))
+        os.system("cp %s %s" %(self.target_pdb.get().getFileName(), self._getExtraPath("target.pdb")))
 
         pre, ext = os.path.splitext(os.path.basename(fnVolume))
         if ext != ".mrc":
